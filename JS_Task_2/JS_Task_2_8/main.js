cd93a99796794272f007d8c9ec1d31d95f892340
besha1313 <besha1313@gmail.com>
2025-02-06 21:57:02 +0200

    add task 2_9

--- JS_Task_2/JS_Task_2_8/main.js
+++ JS_Task_2/JS_Task_2_8/main.js
@@ -9,7 +9,7 @@ let day = +prompt('Введіть число від 1 до 31');
 if (day >= 1 && day <= 10){
     console.log(`Число ${day} знаходиться в І декаді місяця`)
 }else if (day >= 11 && day <= 20){
-    console.log(`Число ${time} знаходиться в ІI декаді місяця`)
+    console.log(`Число ${day} знаходиться в ІI декаді місяця`)
 }else if (day >= 21 && day <= 31){
     console.log(`Число ${day} знаходиться в ІII декаді місяця`)
 }else {
